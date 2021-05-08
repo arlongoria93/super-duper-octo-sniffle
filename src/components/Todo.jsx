a2ac390923115aea725a959a259674b2a66e83c4
Angelo Longoria <arlongoria93@gmail.com>
2021-05-08 14:02:39 -0500

    setting up useState hook with backdrop and modal

---
+++ src/components/Todo.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
+const Todo = (props) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const deleteHandler = () => {
+    setModalIsOpen(true);
+  };
+  return (
+    <div className="card">
+      <h2>{props.text}</h2>
+      <div className="actions">
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+};
+export default Todo;
