b36e5f3c630b4cc0a80a39decb5af18e71b4323e
Angelo Longoria <arlongoria93@gmail.com>
2021-05-10 21:26:55 -0700

    added layout component,added styling

--- src/App.js
+++ src/App.js
@@ -3,23 +3,24 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import Favorites from "./pages/Favorites";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
     <>
-      <MainNavigation />
-      <Switch>
-        <Route path="/" exact>
-          <AllMeetupsPage />
-        </Route>
-        <Route path="/new-meetups">
-          <NewMeetupPage />
-        </Route>
-        <Route path="/favorites">
-          <Favorites />
-        </Route>
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route path="/" exact>
+            <AllMeetupsPage />
+          </Route>
+          <Route path="/new-meetups">
+            <NewMeetupPage />
+          </Route>
+          <Route path="/favorites">
+            <Favorites />
+          </Route>
+        </Switch>
+      </Layout>
     </>
   );
 }
