fc0b3cf1b347825a9f38c0fca93bf40109479a58
Angelo Longoria <arlongoria93@gmail.com>
2021-05-14 17:48:52 -0700

    s

--- src/pages/NewMeetup.jsx
+++ src/pages/NewMeetup.jsx
@@ -1,6 +1,10 @@
+import { useHistory } from "react-router-dom";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  const history = useHistory();
+
   function addMeetupHandler(meetupData) {
     fetch(
       "https://acadmindreactcrash-default-rtdb.firebaseio.com/meetups.json",
@@ -11,8 +15,11 @@ function NewMeetupPage() {
           "Content-Type": "application/json",
         },
       }
-    );
+    ).then(() => {
+      history.replace("/");
+    });
   }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
